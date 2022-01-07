522b6aa4f7ddfbee16c15dbb5ca09652247e735c
jhchoi <cjh@codeblock.co.kr>
2022-01-07 16:27:28 +0900

    수정 : CommonV1 Builder 패턴 수정
    <설명>
    
    1. Builder 패턴에서 유효성 검사를 할 수 있도록 수정
    2. 유효성 검사에서 Exception 발생시 이유를 정확히 알 수 있도록 @NonNull이 아닌 Assert로 사용

--- src/main/java/com/juno/loginApi/api/domain/common/CommonV1.java
+++ src/main/java/com/juno/loginApi/api/domain/common/CommonV1.java
@@ -1,16 +1,25 @@
 package com.juno.loginApi.api.domain.common;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.util.Assert;
 
 @Getter
-@Builder
-@AllArgsConstructor
 public class CommonV1<T> {
     private String result;  //success, fail 구분
     private String code;    //api 고유 상태코드
     private String msg;     //반환 메세지
     private T data;         //반환 데이터
 
+    @Builder
+    public CommonV1(String result, String code, String msg, T data) {
+        Assert.hasText(result, "result 값이 비어있습니다.");
+        Assert.hasText(code, "code 값이 비어있습니다.");
+        Assert.notNull(data, "data 값이 비어있습니다.");
+
+        this.result = result;
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
 }
