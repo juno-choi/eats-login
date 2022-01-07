8448a2473e18f585007eb9aee33655d7f3075580
jhchoi <cjh@codeblock.co.kr>
2022-01-07 14:07:40 +0900

    추가 : security 설정
    <설명>
    
    1. setting > security 패키지를 생성하여 security 설정을 수정함.

---
+++ src/main/java/com/juno/loginApi/setting/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.juno.loginApi.setting.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable() // rest api 이므로 기본설정 사용안함. 기본설정은 비인증시 로그인폼 화면으로 리다이렉트 된다.
+                .csrf().disable() // rest api이므로 csrf 보안이 필요없으므로 disable처리.
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt token으로 인증하므로 세션은 필요없으므로 생성안함.
+                .and()
+                .authorizeRequests() // 다음 리퀘스트에 대한 사용권한 체크
+                .antMatchers("/**").permitAll(); // 가입 및 인증 주소는 누구나 접근가능
+                //.antMatchers("/v1/**").permitAll() // 가입 및 인증 주소는 누구나 접근가능
+                //.anyRequest().hasRole("USER"); // 그외 나머지 요청은 모두 인증된 회원만 접근 가능
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+    }
+}
