04cc91bd4d0d0e4be31fab3236334547ae180dc7
juno-choi <ililil9482@naver.com>
2022-01-09 13:42:47 +0900

    추가 : spring rest docs 추가
    <설명>
    1. gradle에 rest docs 설정 추가
    2. /src/docs/asciidoc 하위 rest docs 정의 파일 작성
    3. test용 controller와 test code 작성법 작성

--- src/main/java/com/juno/loginApi/api/controller/TestController.java
+++ src/main/java/com/juno/loginApi/api/controller/TestController.java
@@ -2,12 +2,13 @@ package com.juno.loginApi.api.controller;
 
 import com.juno.loginApi.api.domain.common.CommonV1;
 import com.juno.loginApi.api.exception.CommonException;
+import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.net.http.HttpResponse;
 
 @RestController
 public class TestController {
@@ -26,7 +27,6 @@ public class TestController {
 
     @GetMapping("/v1/error")
     public ResponseEntity error(){
-
         CommonV1.CommonV1Builder<Object> builder = CommonV1.builder();
         builder.result("success");
         builder.code("200");
@@ -38,4 +38,23 @@ public class TestController {
         if(true) throw new CommonException("내가 만든 exception", HttpStatus.FOUND);
         return new ResponseEntity(result, HttpStatus.OK);
     }
+
+    @PostMapping("/v1/rest_docs")
+    public ResponseEntity restDocs(@RequestBody RestDocsDto rdd){
+
+        CommonV1.CommonV1Builder<Object> builder = CommonV1.builder();
+        builder.result("success");
+        builder.code("200");
+        builder.data(rdd);
+        builder.msg("성공");
+
+        return ResponseEntity.status(HttpStatus.OK).body(builder.build());
+    }
+
+    @Data
+    public static class RestDocsDto {
+        String id;
+        String pw;
+    }
+
 }
